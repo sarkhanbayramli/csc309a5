8e15c84cf03d9e059d45d1a9347a62bec8d64690
kredd <allahverdi.suleymanov@gmail.com>
2015-11-28 14:25:53 -0500

    Added register/login wireframe, fixed homepage view

--- sodasopa/routes/index.js
+++ sodasopa/routes/index.js
@@ -7,4 +7,17 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
+/* GET login/register page. */
+router.get('/login', function(req, res, next) {
+  res.render('login.html');
+});
+
+//Ignores the favicon.ico get requests.
+router.get('/favicon.ico', function(req, res, next) {
+    res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+    res.end();
+    console.log('Favicon request');
+    return;
+});
+
 module.exports = router;
