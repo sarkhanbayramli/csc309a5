f409ac81508a7cf0a40b6d60df413a218259c300
kredd <allahverdi.suleymanov@gmail.com>
2015-11-29 20:16:01 -0500

    Added user view

--- sodasopa/routes/index.js
+++ sodasopa/routes/index.js
@@ -12,6 +12,16 @@ router.get('/login', function(req, res, next) {
   res.render('login.html');
 });
 
+/* GET a specific business's page. */
+router.get('/business/:id', function(req, res, next) {
+  res.render('business.html');
+});
+
+/* GET a specific user. */
+router.get('/user/:id', function(req, res, next) {
+  res.render('user.html');
+});
+
 //Ignores the favicon.ico get requests.
 router.get('/favicon.ico', function(req, res, next) {
     res.writeHead(200, {'Content-Type': 'image/x-icon'} );
